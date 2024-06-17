32cc5fbb61882e27ba541dbbaa7f21c86443da86
rambatinoo <barrywjames1990@gmail.com>
2024-06-17 11:31:11 +0100

    adding header and Nav bar to appear on all routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo_header">
+        <img alt="LOGO HERE"></img>
+        <h1>NC News</h1>
+      </div>
+
+      <p>all the gossip from your local community</p>
+    </div>
+  );
+};
