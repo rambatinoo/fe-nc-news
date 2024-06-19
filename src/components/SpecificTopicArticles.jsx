2e1a510f345d08637bc6f569ba55869939ca98ba
rambatinoo <barrywjames1990@gmail.com>
2024-06-19 13:30:51 +0100

    added functionality for article cards to appear for the selected topic

--- src/components/SpecificTopicArticles.jsx
+++ src/components/SpecificTopicArticles.jsx
@@ -1,17 +1,40 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getArticles } from "../utils/api";
+import { TopicDropdown } from "./TopicDropdown";
+import { ArticleCard } from "./ArticleCard";
 
 export const SpecificTopicArticles = () => {
   const { topic } = useParams();
   const [articles, setArticles] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
 
   useEffect(() => {
     console.log(topic);
     getArticles(topic).then((response) => {
-      console.log(response);
+      setArticles(response.articles);
+      setTotalCount(response.totalCount);
     });
   }, []);
 
-  return <h1>{topic} Articles</h1>;
+  return (
+    <div>
+      <label>
+        You are viewing articles relating to {topic}, want to change?
+        <TopicDropdown />
+      </label>
+      <p>there are {totalCount} articles matching your search</p>
+      <ul className="articles_list">
+        {articles.map((article) => {
+          return (
+            <div>
+              <li className="article_card" key={article.article_id}>
+                <ArticleCard article={{ article }} />
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
