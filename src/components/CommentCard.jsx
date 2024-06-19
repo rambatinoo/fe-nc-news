2e5065b255a20b85193f31bc94ab06dd2096bf1c
rambatinoo <barrywjames1990@gmail.com>
2024-06-19 08:08:12 +0100

    added delete button toconditionally render if the logged in user is the same as the comment author

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,6 +1,11 @@
 import { format } from "date-fns";
+import { useState, useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 export const CommentCard = ({ comment }) => {
+  const { user } = useContext(UserContext);
+  const canDelete = comment.author === user.username;
+
   return (
     <li key={comment.comment_id} className="comment_list_item">
       <div className="comment_card">
@@ -11,6 +16,7 @@ export const CommentCard = ({ comment }) => {
             <p>{comment.votes}</p>
           </div>
           <p>By {comment.author}</p>
+          {canDelete && <button>Delete</button>}
         </div>
       </div>
     </li>
