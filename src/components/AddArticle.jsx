0b7490544a80ae8da389f73d1a8d2b91952a1d95
rambatinoo <barrywjames1990@gmail.com>
2024-08-06 14:52:58 +0100

    add ability to select topic from existing topics

--- src/components/AddArticle.jsx
+++ src/components/AddArticle.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from "react";
+import { getTopics } from "../utils/api";
+import Select from "react-select";
+
 export const AddArticle = () => {
+  const [topic, setTopic] = useState("");
   let topicSelected = false;
+
+  const [options, setOptions] = useState([]);
+  useEffect(() => {
+    getTopics()
+      .then((topics) => {
+        const newOptions = topics.map((topic) => {
+          return { value: topic.slug, label: topic.slug };
+        });
+        setOptions(newOptions);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, []);
+
+  const handleTopicSelect = (selectedTopic) => {
+    setTopic(selectedTopic.value);
+    console.log(topic); //set topic is async so the log is one step behind the topic selected.
+  };
+
   return (
     <div>
       {!topicSelected ? (
         <div>
           <h3>Please select a topic</h3>
+          <Select options={options} onChange={handleTopicSelect} />
           <label>
             or create a new topic
             <input type="checkbox" />
