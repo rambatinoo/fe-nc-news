f08555ee804e93fde66b10a575e152c762bcc6f9
rambatinoo <barrywjames1990@gmail.com>
2024-08-20 11:42:36 +0100

    add function to validate image to the URL - needs testing

--- src/components/AddArticle.jsx
+++ src/components/AddArticle.jsx
@@ -5,7 +5,9 @@ import Select from "react-select";
 export const AddArticle = () => {
   const [topic, setTopic] = useState("");
   const [title, setTitle] = useState("");
+  const [imageURL, setImageURL] = useState("");
   const [titleError, setTitleError] = useState("");
+  const [URLError, setURLError] = useState("");
   let topicSelected = false;
 
   const [options, setOptions] = useState([]);
@@ -37,6 +39,18 @@ export const AddArticle = () => {
       return false;
     } else {
       setTitleError("");
+      return true;
+    }
+  };
+
+  const validateImageURL = () => {
+    const regex =
+      /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/;
+    if (!regex.test(imageURL)) {
+      setURLError("please enter a valid URL");
+      return false;
+    } else {
+      setURLError("");
       return true;
     }
   };
@@ -80,7 +94,11 @@ export const AddArticle = () => {
           <label>
             {" "}
             Image URL:
-            <input required />
+            <input
+              value={imageURL}
+              onChange={(e) => setImageURL(e.target.value)}
+              required
+            />
           </label>
         </div>
         <label>
